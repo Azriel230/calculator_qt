55b2e6c8902a1d309ac38d6d0829d1011f252c1f
Alexey <91151767+Azriel230@users.noreply.github.com>
2023-03-09 15:24:49 +0300

    Add files via upload

--- mymainwindow.cpp
+++ mymainwindow.cpp
@@ -13,3 +13,91 @@ MyMainWindow::~MyMainWindow()
     delete ui;
 }
 
+void MyMainWindow::display_res()
+{
+    ui->m_lcd_result->display(m_display);
+}
+
+void MyMainWindow::on_m_pb_0_clicked()
+{
+    m_display += '0';
+    display_res();
+}
+
+
+void MyMainWindow::on_m_pb_1_clicked()
+{
+    m_display += '1';
+    display_res();
+}
+
+
+void MyMainWindow::on_m_pb_2_clicked()
+{
+    m_display += '2';
+    display_res();
+}
+
+void MyMainWindow::on_m_pb_3_clicked()
+{
+    m_display += '3';
+    display_res();
+}
+
+void MyMainWindow::on_m_pb_4_clicked()
+{
+    m_display += '4';
+    display_res();
+}
+
+void MyMainWindow::on_m_pb_5_clicked()
+{
+    m_display += '5';
+    display_res();
+}
+
+void MyMainWindow::on_m_pb_6_clicked()
+{
+    m_display += '6';
+    display_res();
+}
+
+void MyMainWindow::on_m_pb_7_clicked()
+{
+    m_display += '7';
+    display_res();
+}
+
+void MyMainWindow::on_m_pb_8_clicked()
+{
+    m_display += '8';
+    display_res();
+}
+
+void MyMainWindow::on_m_pb_9_clicked()
+{
+    m_display += '9';
+    display_res();
+}
+
+void MyMainWindow::on_m_pb_Backspace_clicked()
+{
+    m_display.chop(1);
+    display_res();
+}
+
+
+void MyMainWindow::on_m_pb_Clear_clicked()
+{
+    m_display.clear();
+    display_res();
+}
+
+
+void MyMainWindow::on_m_pb_Clear_All_clicked()
+{
+    m_display.clear();
+    m_res = 0;
+    display_res();
+}
+
--- mymainwindow.h
+++ mymainwindow.h
@@ -2,10 +2,13 @@
 #define MYMAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MyMainWindow; }
 QT_END_NAMESPACE
+
+enum Operations {plus, minus, mult, divide};
 
 class MyMainWindow : public QMainWindow
 {
@@ -14,6 +17,43 @@ class MyMainWindow : public QMainWindow
 public:
     MyMainWindow(QWidget *parent = nullptr);
     ~MyMainWindow();
+
+private slots:
+    void on_m_pb_0_clicked();
+
+    void on_m_pb_1_clicked();
+
+    void on_m_pb_2_clicked();
+
+    void on_m_pb_3_clicked();
+
+    void on_m_pb_4_clicked();
+
+    void on_m_pb_5_clicked();
+
+    void on_m_pb_6_clicked();
+
+    void on_m_pb_7_clicked();
+
+    void on_m_pb_8_clicked();
+
+    void on_m_pb_9_clicked();
+
+
+    void on_m_pb_Backspace_clicked();
+
+    void on_m_pb_Clear_clicked();
+
+    void on_m_pb_Clear_All_clicked();
+
+private:
+    void display_res();
+
+private:
+    QString m_display;
+    QString m_display_memory;
+
+    double m_res = 0;
 
 private:
     Ui::MyMainWindow *ui;
