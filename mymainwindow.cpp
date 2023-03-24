fcc007a51760e2ed85cc2e02681a36a351132c9e
Alexey <91151767+Azriel230@users.noreply.github.com>
2023-03-24 18:47:19 +0300

    Update mymainwindow.cpp

--- mymainwindow.cpp
+++ mymainwindow.cpp
@@ -15,97 +15,141 @@ MyMainWindow::~MyMainWindow()
     delete ui;
 }
 
+/*
+* Выводит на LCD Number введеное число
+*/
 void MyMainWindow::display_res()
 {
     ui->m_lcd_result->display(m_display);
 }
-
+/*
+* Выводит в label число, которое находится в памяти калькулятора
+*/
 void MyMainWindow::display_memory_res()
 {
     ui->m_label_memory_res->setText("Memory: " + m_display_memory);
 }
 
+/*
+* Выводит в label символ знака последней нажатой операции 
+*/
 void MyMainWindow::display_operation_res()
 {
     ui->m_label_ar_op->setText(m_display_operation);
 }
 
+/*
+* Выводит в лсд цифру 0
+*/
 void MyMainWindow::on_m_pb_0_clicked()
 {
+    // возможно нужно сделать так, чтобы если в lsd находится 0, то новые не печатать
     m_display += '0';
     display_res();
 }
 
-
+/*
+* Выводит в лсд цифру 1
+*/
 void MyMainWindow::on_m_pb_1_clicked()
 {
     m_display += '1';
     display_res();
 }
 
-
+/*
+* Выводит в лсд цифру 2
+*/
 void MyMainWindow::on_m_pb_2_clicked()
 {
     m_display += '2';
     display_res();
 }
 
+/*
+* Выводит в лсд цифру 3
+*/
 void MyMainWindow::on_m_pb_3_clicked()
 {
     m_display += '3';
     display_res();
 }
 
+/*
+* Выводит в лсд цифру 4
+*/
 void MyMainWindow::on_m_pb_4_clicked()
 {
     m_display += '4';
     display_res();
 }
 
+/*
+* Выводит в лсд цифру 5
+*/
 void MyMainWindow::on_m_pb_5_clicked()
 {
     m_display += '5';
     display_res();
 }
 
+/*
+* Выводит в лсд цифру 6
+*/
 void MyMainWindow::on_m_pb_6_clicked()
 {
     m_display += '6';
     display_res();
 }
 
+/*
+* Выводит в лсд цифру 7
+*/
 void MyMainWindow::on_m_pb_7_clicked()
 {
     m_display += '7';
     display_res();
 }
 
+/*
+* Выводит в лсд цифру 8
+*/
 void MyMainWindow::on_m_pb_8_clicked()
 {
     m_display += '8';
     display_res();
 }
 
+/*
+* Выводит в лсд цифру 9
+*/
 void MyMainWindow::on_m_pb_9_clicked()
 {
     m_display += '9';
     display_res();
 }
 
+/*
+* Удаляет последнюю введенную цифру с экрана
+*/
 void MyMainWindow::on_m_pb_Backspace_clicked()
 {
     m_display.chop(1);
     display_res();
 }
 
-
+/*
+* Очищает число, выведенное на экране
+*/
 void MyMainWindow::on_m_pb_Clear_clicked()
 {
     m_display.clear();
     display_res();
 }
 
-
+/*
+* Очищает число, введенное на экране и обнуляет число-результат
+*/
 void MyMainWindow::on_m_pb_Clear_All_clicked()
 {
     m_display.clear();
@@ -113,6 +157,9 @@ void MyMainWindow::on_m_pb_Clear_All_clicked()
     display_res();
 }
 
+/*
+* Очищает число в памяти калькулятора
+*/
 void MyMainWindow::on_m_pb_MC_clicked()
 {
     m_res_memory = 0;
@@ -120,14 +167,18 @@ void MyMainWindow::on_m_pb_MC_clicked()
     display_memory_res();
 }
 
-
+/*
+* Выводит на экран число из памяти калькулятора
+*/
 void MyMainWindow::on_m_pb_MR_clicked()
 {
     m_display = m_display.number(m_res_memory);
     display_res();
 }
 
-
+/*
+* Сохраняет число с экрана в память калькулятора
+*/
 void MyMainWindow::on_m_pb_MS_clicked()
 {
     m_res_memory = m_display.toDouble();
@@ -135,7 +186,9 @@ void MyMainWindow::on_m_pb_MS_clicked()
     display_memory_res();
 }
 
-
+/*
+* Прабавляет число с экрана к числу в памяти
+*/
 void MyMainWindow::on_m_pb_Mplus_clicked()
 {
     m_res_memory += m_display.toDouble();
@@ -143,7 +196,9 @@ void MyMainWindow::on_m_pb_Mplus_clicked()
     display_memory_res();
 }
 
-
+/*
+* Вычисляет квадратный корень числа с экрана и выводит результат
+*/ 
 void MyMainWindow::on_m_pb_Sqrt_clicked()
 {
     m_res = m_display.toDouble();
@@ -152,6 +207,9 @@ void MyMainWindow::on_m_pb_Sqrt_clicked()
     display_res();
 }
 
+/*
+* Запоминает в буфер число с экрана и присваивает флагу операции нужный код (для '+' - 1)
+*/
 void MyMainWindow::on_m_pb_plus_clicked()
 {
     flag_operation = 1;
@@ -162,6 +220,9 @@ void MyMainWindow::on_m_pb_plus_clicked()
     display_res();
 }
 
+/*
+* Запоминает в буфер число с экрана и присваивает флагу операции нужный код (для '-' - 2)
+*/
 void MyMainWindow::on_m_pb_minus_clicked()
 {
     flag_operation = 2;
@@ -172,6 +233,9 @@ void MyMainWindow::on_m_pb_minus_clicked()
     display_res();
 }
 
+/*
+* Запоминает в буфер число с экрана и присваивает флагу операции нужный код (для '*' - 3)
+*/
 void MyMainWindow::on_m_pb_mult_clicked()
 {
     flag_operation = 3;
@@ -182,6 +246,9 @@ void MyMainWindow::on_m_pb_mult_clicked()
     display_res();
 }
 
+/*
+* Запоминает в буфер число с экрана и присваивает флагу операции нужный код (для '/' - 4)
+*/
 void MyMainWindow::on_m_pb_div_clicked()
 {
     flag_operation = 4;
@@ -192,6 +259,9 @@ void MyMainWindow::on_m_pb_div_clicked()
     display_res();
 }
 
+/*
+* Вычисляет выражение в соответствии с кодом операции и выводит результат на экран
+*/
 void MyMainWindow::on_m_pb_equal_clicked()
 {
     switch(flag_operation)
@@ -215,10 +285,11 @@ void MyMainWindow::on_m_pb_equal_clicked()
     flag_operation = 0;
     m_display = m_display.number(m_res);
     display_res();
-
 }
 
-
+/*
+* Возводит число с экрана в квадрат и выводит его на экран
+*/
 void MyMainWindow::on_m_pb_sq_clicked()
 {
     m_res = m_display.toDouble();
@@ -227,7 +298,9 @@ void MyMainWindow::on_m_pb_sq_clicked()
     display_res();
 }
 
-
+/*
+* Делит 1 на число с экрана и выводит результат на экран
+*/
 void MyMainWindow::on_m_pb_fraction_clicked()
 {
     m_res = m_display.toDouble();
@@ -236,7 +309,9 @@ void MyMainWindow::on_m_pb_fraction_clicked()
     display_res();
 }
 
-
+/*
+* Заменяет знак числа с экрана
+*/
 void MyMainWindow::on_m_pb_sign_clicked()
 {
     m_res = m_display.toDouble();
@@ -245,7 +320,9 @@ void MyMainWindow::on_m_pb_sign_clicked()
     display_res();
 }
 
-
+/*
+* Добавляет точку на экран, делая число десятичной дробью
+*/
 void MyMainWindow::on_m_pb_dot_clicked()
 {
     if(!m_display.contains('.'))
